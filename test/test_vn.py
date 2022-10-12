f944443de4869e0b1981fea465d0160ed90f92a5
y-richie-y <richie.yeung1@gmail.com>
2022-10-12 16:48:26 +0100

    Add VN extractor

---
+++ test/test_vn.py
@@ -0,0 +1,16 @@
+import pytest
+from extlib.utils import von_neumann
+
+vn_testcases = [
+    ([], []),
+    ([0, 1, 0, 1], []),
+    ([0, 0, 0, 0], [0, 0]),
+    ([0, 0, 1, 1], [0, 1]),
+    ([1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0],
+     [0, 1, 1, 0, 1])
+]
+
+
+@pytest.mark.parametrize("bits, expect", vn_testcases)
+def test_vn(bits, expect):
+    assert von_neumann(bits) == expect
