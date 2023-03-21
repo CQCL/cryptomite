47bffb748cc86d5de05f574b2ca8bfff6296b1e0
y-richie-y <richie.yeung1@gmail.com>
2023-03-21 14:38:33 +0000

    Move extractors to top level

--- cryptomite/__init__.py
+++ cryptomite/__init__.py
@@ -1,6 +1,19 @@
 
-__all__ = ['dodis', 'toeplitz', 'trevisan']
+__all__ = [
+    'dodis',
+    'toeplitz',
+    'trevisan',
+    'utils',
+    'Dodis',
+    'Toeplitz',
+    'Trevisan',
+    'von_neumann'
+]
 
-from cryptomite import dodis, toeplitz, trevisan
+from cryptomite import dodis, toeplitz, trevisan, utils
+from cryptomite.dodis import Dodis
+from cryptomite.toeplitz import Toeplitz
+from cryptomite.trevisan import Trevisan
+from cryptomite.utils import von_neumann
 
 __version__ = '0.1.0'
