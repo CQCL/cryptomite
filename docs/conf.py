5db920fb2a7f65ad08963bdaba63d9b24244b852
cforeman-cqc <cameron.foreman@cambridgequantum.com>
2023-12-22 21:08:49 +0000

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = '_static/images/logo.png'
+html_logo = '_static/figures/cryptomite_logo_temp.png'
 html_favicon = '_static/images/favicon.ico'
 
 numfig = True
